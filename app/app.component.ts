51b092b404b807f92a663a28d9b742872604e96a
Sheard Z <zsheng12@gmail.com>
2016-08-13 14:15:39 -0400

    move hero out to a seperate component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,17 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
-export class Hero{
-  id: number;
-  name: string;
-}
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
-  <div *ngIf = "selectedHero">  
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><lable>id: </lable>{{selectedHero.id}}</div>
-    <div>
-      <lable>name: </lable>
-      <input [(ngModel)]="selectedHero.name" placeholder="name">
-    </div>
-  </div>
+  <my-hero-detail [hero] = 'selectedHero'></my-hero-detail>
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor = "let hero of heroes" [class.selected]= "hero === selectedHero" (click)="onSelect(hero)">
+    <li *ngFor = "let hero of heroes" 
+        [class.selected]= "hero === selectedHero" 
+        (click)="onSelect(hero)">
       <span class="badge"> {{hero.id}}</span>{{hero.name}}
     </li>
   </ul>
