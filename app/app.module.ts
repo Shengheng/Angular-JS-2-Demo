5ca330abe8a9e1b7b573faf80236306b22aa4ab5
Sheard Z <zsheng12@gmail.com>
2016-08-15 21:44:11 -0400

    http WebAPI service simulation init

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule }        from '@angular/core';
 import { BrowserModule }   from '@angular/platform-browser';
 import { FormsModule }     from '@angular/forms';
 
+//Imports for loading & configuring the in-meomory APU
+import { HttpModule, XHRBackend } from '@angular/http';
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppComponent }    from './app.component';
 import { routing }         from './app.routing';
 
@@ -15,7 +20,8 @@ import { HeroService }     from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
-    routing
+    routing,
+    HttpModule
   ],
   //decorator's declarations array for all components, pipes, and directives we created
   declarations: [
